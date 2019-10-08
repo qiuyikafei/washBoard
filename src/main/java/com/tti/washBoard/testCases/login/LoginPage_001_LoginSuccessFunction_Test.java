c439a7c56ec803d53d60e9e17611b4381d9a79f6
Cynthia <1140535792qq.com@>
2019-10-08 17:29:28 +0800

    测试git提交

--- src/main/java/com/tti/washBoard/testCases/login/LoginPage_001_LoginSuccessFunction_Test.java
+++ src/main/java/com/tti/washBoard/testCases/login/LoginPage_001_LoginSuccessFunction_Test.java
@@ -18,7 +18,7 @@ public class LoginPage_001_LoginSuccessFunction_Test extends BaseParpare{
       //等待登录页面加载
       LoginPageHelper.waitLoginPageLoad(seleniumUtil, timeOut);
       // 输入登录信息
-      LoginPageHelper.typeLoginInfo(seleniumUtil,"admin@wb02.com", "123456");
+      LoginPageHelper.typeLoginInfo(seleniumUtil,"admin@wb02.com", "1234561");
       //等待首页元素显示出来
       HomePageHelper.waitHomePageLoad(seleniumUtil, timeOut);
       //检查用户名是不是期望的"jojo"
